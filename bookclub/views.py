641361deb8c67d4b45c649c1e7d11f7d7be3df39
Zakariya Ahmed Mohamed <zakariya.ahmed_mohamed@kcl.ac.uk>
2022-01-25 20:46:59 +0000

    Design landing page for book club

--- bookclub/views.py
+++ bookclub/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from bookclub.templates import *
 
 # Create your views here.
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
--- system/settings.py
+++ system/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import bookclub
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookclub'
 ]
 
 MIDDLEWARE = [
