513978a541df107b5a55ae62d1b930101f1e8c2b
Yigit Cengiz <mmyig3@gmail.com>
2022-01-30 20:29:36 +0000

    register model

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'first_name', 'last_name', 'email', 'is_active', 'chess_experience_level',
+    ]
