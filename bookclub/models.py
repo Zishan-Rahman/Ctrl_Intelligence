977bd11cba6fa2275aae63ee171e39ec89b6c700
Yigit Cengiz <mmyig3@gmail.com>
2022-01-30 21:02:35 +0000

    user model created

--- bookclub/models.py
+++ bookclub/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.utils.timezone import make_aware
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.utils.translation import ugettext_lazy as _
 from .custom_managers import UserManager
 # # Create your models here.
 
@@ -18,34 +17,29 @@ class User(AbstractBaseUser, PermissionsMixin):
     # All these field declarations are copied as-is
     # from `AbstractUser`
     first_name = models.CharField(
-        _('first name'),
         max_length=30,
         blank=False,
     )
     last_name = models.CharField(
-        _('last name'),
         max_length=30,
         blank=False,
     )
     is_staff = models.BooleanField(
-        _('staff status'),
         default=False,
-        help_text=_(
+        help_text=(
             'Designates whether the user can log into '
             'this admin site.'
         ),
     )
     is_active = models.BooleanField(
-        _('active'),
         default=True,
-        help_text=_(
+        help_text=(
             'Designates whether this user should be '
             'treated as active. Unselect this instead '
             'of deleting accounts.'
         ),
     )
     date_joined = models.DateTimeField(
-        _('date joined'),
         default=timezone.now,
     )
 
--- system/settings.py
+++ system/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookclub'
+    'bookclub',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +115,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+AUTH_USER_MODEL = 'bookclub.User'
 
 
 # Static files (CSS, JavaScript, Images)
