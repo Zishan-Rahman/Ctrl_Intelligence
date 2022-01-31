8355ff6f9632b33d0f240432f0e12fad7d8c91b2
brendonzoto <brendon.zoto@kcl.ac.uk>
2022-01-31 12:20:22 +0000

    Modified User model in accordance to the users csv file requirements

--- system/urls.py
+++ system/urls.py
@@ -19,8 +19,8 @@ from bookclub import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.landing_page, name= 'landing_page'),
-    path('home_page/',views.home_page , name = 'home_page'),
-    path('user_list/',views.user_list , name = 'user_list'),
-    path('club_list/',views.club_list , name = 'club_list'),
+    path('', views.landing_page, name='landing_page'),
+    path('home/', views.home_page, name='home_page'),
+    path('users/', views.user_list, name='user_list'),
+    path('clubs/', views.club_list, name='club_list'),
 ]
