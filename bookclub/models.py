79544f7612f4c26016aa113770d8ba0cce3a4e43
Fathima Jamal-Deen <fathima.jamal-deen@kcl.ac.uk>
2022-01-31 18:11:01 +0000

    Add to get help with login faults

--- bookclub/models.py
+++ bookclub/models.py
@@ -4,11 +4,10 @@ from django.utils.timezone import make_aware
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from .custom_managers import UserManager
-
-
 # # Create your models here.
 
 class User(AbstractBaseUser, PermissionsMixin):
+
     email = models.EmailField(
         unique=True,
         max_length=255,
@@ -46,68 +45,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # Add additional fields here if needed
 
-    # Had to remove blank=True for public_bio in order to edit it?
-    public_bio = models.CharField(max_length=520, blank=True)
+    #Had to remove blank=True for public_bio in order to edit it?
+    public_bio = models.CharField(max_length=520)
 
-    favourite_genre = models.CharField(max_length=30, blank=True)
-
-    location = models.CharField(max_length=30, blank=False, default=None)
-
-    age = models.IntegerField(default=None)
+    short_personal_statement = models.TextField()
 
     def full_name(self):
-        return f'{self.first_name} {self.last_name}'
+      return f'{self.first_name} {self.last_name}'
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
-
-class Club(models.Model):
-    name = models.CharField(unique=True, blank=False, max_length=50)
-    description = models.CharField(blank=True, max_length=500)
-    location = models.CharField(blank=False, max_length=100)
-    members = models.ManyToManyField(User, related_name='member_of')
-    organisers = models.ManyToManyField(User, related_name='organiser_of')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_of')
-
-    def get_name(self):
-        return self.name
-
-    def get_description(self):
-        return self.description
-
-    def get_location(self):
-        return self.location
-
-    def user_level(self, user):
-        if self.owner == user:
-            return "Owner"
-        elif self.organisers.filter(email=user.email):
-            return "Organiser"
-        elif self.members.filter(email=user.email):
-            return "Member"
-        else:
-            return "Not in club"
-
-    def make_owner(self, user):
-        if self.user_level(user) == "Organiser":
-            self.organisers.remove(user)
-            self.organisers.add(self.owner)
-            self.owner = user
-            self.save()
-        else:
-            raise ValueError
-
-    def make_organiser(self, user):
-        if self.user_level(user) == "Member":
-            self.members.remove(user)
-            self.organisers.add(user)
-            self.save()
-        else:
-            raise ValueError
-
-    def make_member(self, user):
-        self.members.add(user)
-        self.save()
-
--- system/settings.py
+++ system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookclub',
+    'widget_tweaks',
 
 ]
 
