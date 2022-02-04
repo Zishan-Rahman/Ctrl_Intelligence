b0bff600efdd785292a25c5473501874c2ea7697
Zishan Rahman <zishanfrahman@outlook.com>
2022-02-04 21:52:05 +0000

    FIX ALL THE TESTS (and add a few new ones)!

--- bookclub/tests/models/test_application_model.py
+++ bookclub/tests/models/test_application_model.py
@@ -8,18 +8,28 @@ class ApplicationModelTestCase(TestCase):
         "bookclub/tests/fixtures/default_users.json",
     ]
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.user_one = User.objects.get(pk=1)
-        self.user_two = User.objects.get(pk=2)
-        self.club_bush_house = Club.objects.get(pk=1)
         self.club_somerset_house = Club.objects.get(pk=2)
+        self.application = Application.objects.create(self.user_one, self.club_somerset_house)
         
-    def _assert_application_is_valid(self, application: Application) -> None:
+    def _assert_application_is_valid(self) -> None:
         try:
-            application.full_clean()
+            self.application.full_clean()
         except ValidationError:
             self.fail("Application is not valid")
             
-    def _assert_application_is_invalid(self, application: Application) -> None:
+    def _assert_application_is_invalid(self) -> None:
         with self.assertRaises(ValidationError):
-            application.full_clean()
+            self.application.full_clean()
+            
+    def test_new_application(self) -> None:
+        self._assert_application_is_valid
+        
+    def test_applicant_must_be_present(self) -> None:
+        self.application.applicant = None
+        self._assert_application_is_invalid
+        
+    def test_club_must_be_present(self) -> None:
+        self.application.club = None
+        self._assert_application_is_invalid
