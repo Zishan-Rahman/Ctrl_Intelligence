f404a958b112dd7248fce8b22e66f08a6b1ccbc3
Fathima Jamal-Deen <fathima.jamal-deen@kcl.ac.uk>
2022-02-10 23:09:36 +0000

    List of all available clubs

--- bookclub/views/club_views.py
+++ bookclub/views/club_views.py
@@ -4,9 +4,16 @@ from bookclub.forms import ClubForm
 from bookclub.models import Club
 
 @login_required
-def clubs_list(request):
-    clubs = Club.objects.all()
+def club_list(request):
+    clubs = []
+    for club in Club.objects.all():
+        clubs.append({
+            "name": club.get_name,
+            "description": club.get_description,
+            "location": club.get_location,
+        })
     return render(request, 'club_list.html', {'clubs':clubs})
+
 @login_required
 def new_club(request):     # new club adapted from the chess club project
     """ Create New Club """
