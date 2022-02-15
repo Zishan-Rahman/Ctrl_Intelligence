e6f61107f9b62b582b19abcb1dfb80730bb66b19
Zishan Rahman <zishanfrahman@outlook.com>
2022-02-15 20:41:07 +0000

    Remove commented out code in club profile

--- bookclub/views/club_views.py
+++ bookclub/views/club_views.py
@@ -35,23 +35,4 @@ def new_club(request):     # new club adapted from the chess club project
 def club_profile(request, club_id):
     """ Individual Club's Profile Page """
     club = Club.objects.get(id = club_id)
-    #owner= Ownership.objects.get(club = club.id).owner
-    #members = []
-    #for membership in Membership.objects.all().filter(club = club):
-        #for member in membership.member.all():
-            #members.append(member)
-
-    # officers = []
-    # for worksfor in WorksFor.objects.all().filter(club = club):
-    #     for officer in worksfor.officer.all():
-    #         officers.append(officer)
-
-
-    #clubs_already_applied = []
-    #for application in Application.objects.all():
-        #if application.applicant_id == request.user.id:
-            #clubs_already_applied.append(application.club)
-
-    # members_number = Membership.objects.all().filter(club=club).count() + WorksFor.objects.all().filter(club=club).count() + 1
-
     return render(request, 'club_profile.html',{'club':club})
