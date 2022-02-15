140bbad70b11ff950e8d876229c83ecd36509795
brendonzoto <brendon.zoto@kcl.ac.uk>
2022-02-15 11:50:47 +0000

    Implement cards system for club switcher

--- bookclub/views/club_views.py
+++ bookclub/views/club_views.py
@@ -3,28 +3,31 @@ from django.shortcuts import redirect, render
 from bookclub.forms import ClubForm
 from bookclub.models import Club
 
-@login_required
-def club_list(request):
-    memberships = Club.objects.filter(members=request.user) | Club.objects.filter(organisers=request.user) | Club.objects.filter(owner=request.user)
-    clubs = []
-    for club in Club.objects.all():
-        clubs.append({
-            "name": club.get_name,
-            "description": club.get_description,
-            "location": club.get_location,
-            "mini_gravatar": club.mini_gravatar(),
-            "gravatar": club.gravatar()
-        })
-    return render(request, 'club_list.html', {'clubs':clubs, "club_memberships": memberships})
 
 @login_required
-def new_club(request):     # new club adapted from the chess club project
+def club_list(request):
+    clubs = Club.objects.all()
+    return render(request, 'club_list.html', {'clubs': clubs})
+
+
+def club_selector(request):
+    user_clubs = []
+    clubs = Club.objects.all()
+
+    for temp_club in clubs:
+        if request.user in temp_club.get_all_users():
+            user_clubs.append(temp_club)
+    return render(request, "club_switcher.html", {'user_clubs': user_clubs})
+
+
+@login_required
+def new_club(request):  # new club adapted from the chess club project
     """ Create New Club """
     if request.method == 'POST':
         form = ClubForm(request.POST)
         if form.is_valid():
-            club = form.save(request.user)
+            form.save(request.user)
             return redirect('club_list')
     else:
         form = ClubForm()
-    return render(request, 'new_club.html', {'form':form})
+    return render(request, 'new_club.html', {'form': form})
