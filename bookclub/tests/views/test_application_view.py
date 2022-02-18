cf9411e7c19b773d84e345927e783072029fa278
brendonzoto <k19011443@kcl.ac.uk>
2022-02-18 14:58:08 +0000

    Remove pagination for application and comment out test for it

--- bookclub/tests/views/test_application_view.py
+++ bookclub/tests/views/test_application_view.py
@@ -10,7 +10,8 @@ from django.contrib import messages
 class ApplicationViewTestCase(TestCase):
     """Tests of the application view."""
 
-    fixtures = ['bookclub/tests/fixtures/default_users.json', 'bookclub/tests/fixtures/default_clubs.json', 'bookclub/tests/fixtures/default_applications.json']
+    fixtures = ['bookclub/tests/fixtures/default_users.json', 'bookclub/tests/fixtures/default_clubs.json',
+                'bookclub/tests/fixtures/default_applications.json']
 
     def setUp(self):
         self.url = reverse('applications')
@@ -23,9 +24,8 @@ class ApplicationViewTestCase(TestCase):
         self.somerset_club = Club.objects.get(name='Somerset House Book Club')
         self.strand_club = Club.objects.get(name='Strand House Book Club')
 
-
     def test_application_url(self):
-        self.assertEqual(self.url,'/applications/')
+        self.assertEqual(self.url, '/applications/')
 
     def test_application_uses_correct_template(self):
         self.client.login(email=self.joe.email, password='Password123')
@@ -37,14 +37,14 @@ class ApplicationViewTestCase(TestCase):
         self.client.login(email=self.jane.email, password='Password123')
         response = self.client.get(reverse('applications'))
         html = response.content.decode('utf8')
-        self.assertIn('<td>Somerset House Book Club</td>',  html)
-        self.assertIn('<td>Joe</td>',  html)
-        self.assertIn('<td>Doe</td>',  html)
+        self.assertIn('<td>Somerset House Book Club</td>', html)
+        self.assertIn('<td>Joe</td>', html)
+        self.assertIn('<td>Doe</td>', html)
         self.assertIn('<td>30</td>', html)
-        self.assertIn('<td>Just another fake user again</td>',  html)
-        self.assertIn('<td>Romance</td>',  html)
-        self.assertIn('<td>Manchester</td>',  html)
-        self.assertIn('<a class="btn btn-default"',  html)
+        self.assertIn('<td>Just another fake user again</td>', html)
+        self.assertIn('<td>Romance</td>', html)
+        self.assertIn('<td>Manchester</td>', html)
+        self.assertIn('<a class="btn btn-default"', html)
 
     def test_no_applications(self):
         self.client.login(email=self.joe.email, password='Password123')
@@ -61,45 +61,46 @@ class ApplicationViewTestCase(TestCase):
         self.client.login(email=self.jane.email, password='Password123')
         response = self.client.get(reverse('applications'))
         html = response.content.decode('utf8')
-        self.assertIn('<td>Somerset House Book Club</td>',  html)
-        self.assertIn('<td>Joe</td>',  html)
-        self.assertIn('<td>Doe</td>',  html)
-        self.assertIn('<td>30</td>',  html)
-        self.assertIn('<td>Just another fake user again</td>',  html)
-        self.assertIn('<td>Romance</td>',  html)
-        self.assertIn('<td>Manchester</td>',  html)
-        self.assertIn('<a class="btn btn-default"',  html)
+        self.assertIn('<td>Somerset House Book Club</td>', html)
+        self.assertIn('<td>Joe</td>', html)
+        self.assertIn('<td>Doe</td>', html)
+        self.assertIn('<td>30</td>', html)
+        self.assertIn('<td>Just another fake user again</td>', html)
+        self.assertIn('<td>Romance</td>', html)
+        self.assertIn('<td>Manchester</td>', html)
+        self.assertIn('<a class="btn btn-default"', html)
 
-        self.assertIn('<td>Somerset House Book Club</td>',  html)
-        self.assertIn('<td>John</td>',  html)
-        self.assertIn('<td>Doe</td>',  html)
-        self.assertIn('<td>39</td>',  html)
-        self.assertIn("<td>I&#x27;m just an abstract concept!</td>",  html) #Apostrophe converted to &#x27;m in html source code
-        self.assertIn('<td>Science fiction</td>',  html)
-        self.assertIn('<td>London</td>',  html)
-        self.assertIn('<a class="btn btn-default"',  html)
+        self.assertIn('<td>Somerset House Book Club</td>', html)
+        self.assertIn('<td>John</td>', html)
+        self.assertIn('<td>Doe</td>', html)
+        self.assertIn('<td>39</td>', html)
+        self.assertIn("<td>I&#x27;m just an abstract concept!</td>",
+                      html)  # Apostrophe converted to &#x27;m in html source code
+        self.assertIn('<td>Science fiction</td>', html)
+        self.assertIn('<td>London</td>', html)
+        self.assertIn('<a class="btn btn-default"', html)
 
     def test_multiple_applications_to_different_clubs(self):
         self.client.login(email=self.john.email, password='Password123')
         response = self.client.get(reverse('applications'))
         html = response.content.decode('utf8')
-        self.assertIn('<td>Bush House Book Club</td>',  html)
-        self.assertIn('<td>Joe</td>',  html)
-        self.assertIn('<td>Doe</td>',  html)
-        self.assertIn('<td>30</td>',  html)
-        self.assertIn('<td>Just another fake user again</td>',  html)
-        self.assertIn('<td>Romance</td>',  html)
-        self.assertIn('<td>Manchester</td>',  html)
-        self.assertIn('<a class="btn btn-default"',  html)
+        self.assertIn('<td>Bush House Book Club</td>', html)
+        self.assertIn('<td>Joe</td>', html)
+        self.assertIn('<td>Doe</td>', html)
+        self.assertIn('<td>30</td>', html)
+        self.assertIn('<td>Just another fake user again</td>', html)
+        self.assertIn('<td>Romance</td>', html)
+        self.assertIn('<td>Manchester</td>', html)
+        self.assertIn('<a class="btn btn-default"', html)
 
-        self.assertIn('<td>Strand House Book Club</td>',  html)
-        self.assertIn('<td>Joe</td>',  html)
-        self.assertIn('<td>Doe</td>',  html)
-        self.assertIn('<td>30</td>',  html)
-        self.assertIn('<td>Just another fake user again</td>',  html)
-        self.assertIn('<td>Romance</td>',  html)
-        self.assertIn('<td>Manchester</td>',  html)
-        self.assertIn('<a class="btn btn-default"',  html)
+        self.assertIn('<td>Strand House Book Club</td>', html)
+        self.assertIn('<td>Joe</td>', html)
+        self.assertIn('<td>Doe</td>', html)
+        self.assertIn('<td>30</td>', html)
+        self.assertIn('<td>Just another fake user again</td>', html)
+        self.assertIn('<td>Romance</td>', html)
+        self.assertIn('<td>Manchester</td>', html)
+        self.assertIn('<a class="btn btn-default"', html)
 
     def test_successful_accept(self):
         self.client.login(email=self.john.email, password='Password123')
@@ -111,7 +112,7 @@ class ApplicationViewTestCase(TestCase):
         self.assertEqual(len(messages_list), 1)
         self.assertEqual(messages_list[0].level, messages.SUCCESS)
         afterCount = self.strand_club.get_number_of_members()
-        self.assertEqual(beforeCount, afterCount-1)
+        self.assertEqual(beforeCount, afterCount - 1)
 
     def test_successful_reject(self):
         self.client.login(email=self.john.email, password='Password123')
@@ -130,9 +131,10 @@ class ApplicationViewTestCase(TestCase):
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
-    def test_get_application_list_with_pagination(self):
+
+    """ def test_get_application_list_with_pagination(self):
         self.client.login(email=self.user.email, password='Password123')
-        self._create_test_applications(settings.APPLICATIONS_PER_PAGE*2+3-1)
+        self._create_test_applications(settings.APPLICATIONS_PER_PAGE * 2 + 3 - 1)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'applications.html')
@@ -164,11 +166,10 @@ class ApplicationViewTestCase(TestCase):
         self.assertEqual(len(response.context['applicants']), 6)
         page_obj = response.context['page_obj']
         self.assertTrue(page_obj.has_previous())
-        self.assertFalse(page_obj.has_next())
+        self.assertFalse(page_obj.has_next()) """
 
-
-    def _create_test_applications(self,application_count=10):
-        for id in range(1,application_count+1, 1):
+    def _create_test_applications(self, application_count=10):
+        for id in range(1, application_count + 1, 1):
             created_user = User.objects.create(
                 email=f'user{id}@test.org',
                 password='Password123',
@@ -177,9 +178,9 @@ class ApplicationViewTestCase(TestCase):
                 public_bio=f'Bio {id}',
                 favourite_genre=f'genre {id}',
                 location=f'City {id}',
-                age=18+id
+                age=18 + id
             )
-            created_club=Club.objects.create(
+            created_club = Club.objects.create(
                 owner_id=id,
                 name=f'The {id} Book Club',
                 location=f'City {id}',
