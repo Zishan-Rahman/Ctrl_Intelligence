41df54bbc0c7fac837fca50f8625fc0e09584721
Suhayb <91222617+SuhaybYY@users.noreply.github.com>
2022-02-18 12:58:14 +0000

    small changes
    small changes

--- bookclub/tests/forms/test_application_form.py
+++ bookclub/tests/forms/test_application_form.py
@@ -9,33 +9,32 @@ class TestApplicationForm(TestCase):
 
     def setUp(self):
         self.user = User.objects.get(email='johndoe@bookclub.com')
-
-    self.bush_club = Club.objects.get(name='Bush House Book Club')
-    self.bush_club.make_member(self.john)
-    self.form_input = {
-        'applicant': self.user,
-        'club': self.bush_club
-    }
-
-
-def test_application_form_has_necessary_fields(self):
-    form = ApplicationFormForm()
-    self.assertIn('applicant', form.fields)
-    self.assertIn('club', form.fields)
+        self.bush_club = Club.objects.get(name='Bush House Book Club')
+        self.bush_club.make_member(self.john)
+        self.form_input = {
+            'applicant': self.user,
+            'club': self.bush_club
+        }
 
 
-def test_valid_application_form(self):
-    form = ApplicationForm(data=self.form_input)
-    self.assertTrue(form.is_valid())
+    def test_application_form_has_necessary_fields(self):
+        form = ApplicationFormForm()
+        self.assertIn('applicant', form.fields)
+        self.assertIn('club', form.fields)
 
 
-def test_form_rejects_blank_applicant(self):
-    self.form_input['applicant'] = ''
-    form = ApplicationForm(data=self.form_input)
-    self.assertFalse(form.is_valid())
+    def test_valid_application_form(self):
+        form = ApplicationForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
 
 
-def test_form_rejects_blank_club(self):
-    self.form_input['club'] = ''
-    form = ApplicationForm(data=self.form_input)
-    self.assertFalse(form.is_valid())
+    def test_form_rejects_blank_applicant(self):
+        self.form_input['applicant'] = ''
+        form = ApplicationForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+
+    def test_form_rejects_blank_club(self):
+        self.form_input['club'] = ''
+        form = ApplicationForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
