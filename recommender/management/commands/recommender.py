1a720b8c4d718ebca6b0b41ba5b2163ca9313ad5
brendonzoto <k19011443@kcl.ac.uk>
2022-02-21 15:59:45 +0000

    a

---
+++ recommender/management/commands/recommender.py
@@ -0,0 +1,29 @@
+from surprise import SVD
+from surprise import Dataset
+from surprise import Reader
+from surprise.model_selection import cross_validate
+
+import pandas as pd
+
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
+
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        books = pd.read_csv('data/BX_Books.csv', sep=';', on_bad_lines='skip', encoding="ISO-8859-1")
+        books.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageUrlS', 'imageUrlM', 'imageUrlL']
+        users = pd.read_csv('data/BX-Users.csv', sep=';', on_bad_lines='skip', encoding="ISO-8859-1")
+        users.columns = ['userID', 'Location', 'Age']
+        ratings = pd.read_csv('data/BX-Book-Ratings.csv', sep=';', on_bad_lines='skip', encoding="ISO-8859-1")
+        ratings.columns = ['userID', 'ISBN', 'bookRating']
+
+
+
--- system/settings.py
+++ system/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookclub',
     'widget_tweaks',
+    'recommender',
 
 ]
 
