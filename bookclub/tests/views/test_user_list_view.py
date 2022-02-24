319f93b73459fee61c6b19caceb310c2b3bc2f14
Zishan Rahman <zishanfrahman@outlook.com>
2022-02-24 16:04:00 +0000

    Fix one of the tests... but the other test fails for a different reason than it did before

--- bookclub/tests/views/test_user_list_view.py
+++ bookclub/tests/views/test_user_list_view.py
@@ -5,19 +5,13 @@ from bookclub.models import User
 from bookclub.tests.helpers import LogInTester, reverse_with_next
 
 
-class testUserListView(TestCase, LogInTester):
+class TestUserListView(TestCase, LogInTester):
 
     fixtures = ['bookclub/tests/fixtures/default_users.json']
 
     def setUp(self):
         self.url = reverse('user_list')
-        self.user = User.objects.create(
-            first_name = "John",
-            last_name = "John",
-            public_bio = "hfjdsvsk",
-	        email = "johndoe@bookclub.coms",
-            date_joined = "2022-09-04 06:00"
-        )
+        self.user = User.objects.get(email='johndoe@bookclub.com')
 
     def test_user_list_url(self):
         self.assertEqual(self.url,'/users/')
@@ -28,7 +22,7 @@ class testUserListView(TestCase, LogInTester):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "user_list.html")
 
-    def test_get_club_list_with_pagination(self):
+    def test_get_user_list_with_pagination(self):
         self.client.login(email=self.user.email, password='Password123')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
