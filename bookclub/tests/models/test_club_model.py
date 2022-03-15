d2a73849e0468b9ec65cdffeed89477ee9148a88
Suhayb <91222617+SuhaybYY@users.noreply.github.com>
2022-03-15 22:38:37 +0000

    Update test_club_model.py

--- bookclub/tests/models/test_club_model.py
+++ bookclub/tests/models/test_club_model.py
@@ -105,7 +105,7 @@ class ClubModelTestCase(TestCase):
         self.club_temple_house.make_organiser(self.user_two)
         self.assertEqual(club_temple_house.organiser, self.user_two)
         self.club_temple_house.remove_from_club(self.user_two)
-        self.assertNotEqual(club_temple_house.owner, self.user_two)
+        self.assertNotEqual(club_temple_house.organiser, self.user_two)
 
 
     def _assert_book_club_is_valid(self):
