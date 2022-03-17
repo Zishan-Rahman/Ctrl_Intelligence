32219ea6744a4e6d6c1055b311de67215f88e470
brendonzoto <k19011443@kcl.ac.uk>
2022-03-17 22:14:16 +0000

    fix tests for follow system

--- bookclub/tests/views/test_follow_view_follow.py
+++ bookclub/tests/views/test_follow_view_follow.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
-from microblogs.models import User
-from microblogs.tests.helpers import reverse_with_next
+from bookclub.models import User
+from bookclub.tests.helpers import reverse_with_next
 
 class ShowUserTest(TestCase):
 
@@ -15,16 +15,13 @@ class ShowUserTest(TestCase):
     def test_follow_toggle_url(self):
         self.assertEqual(self.url,f'/follow_toggle/{self.followee.id}')
 
-    def test_follow_toggle_url(self):
-        self.assertEqual(self.url,f'/follow_toggle/{self.followee.id}')
-
     def test_get_follow_toggle_redirects_when_not_logged_in(self):
-        redirect_url = reverse_with_next('log_in', self.url)
+        redirect_url = reverse_with_next('login', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_get_follow_toggle_for_followee(self):
-        self.client.login(username=self.user.username, password='Password123')
+        self.client.login(username=self.user.email, password='Password123')
         self.user.toggle_follow(self.followee)
         user_followers_before = self.user.follower_count()
         followee_followers_before = self.followee.follower_count()
@@ -38,7 +35,7 @@ class ShowUserTest(TestCase):
         self.assertTemplateUsed(response, 'user_profile.html')
 
     def test_get_follow_toggle_for_non_followee(self):
-        self.client.login(username=self.user.username, password='Password123')
+        self.client.login(username=self.user.email, password='Password123')
         user_followers_before = self.user.follower_count()
         followee_followers_before = self.followee.follower_count()
         response = self.client.get(self.url, follow=True)
@@ -49,11 +46,3 @@ class ShowUserTest(TestCase):
         response_url = reverse('user_profile', kwargs={'user_id': self.followee.id})
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'user_profile.html')
-
-    def test_get_follow_toggle_with_invalid_id(self):
-        self.client.login(username=self.user.username, password='Password123')
-        url = reverse('follow_toggle', kwargs={'user_id': self.user.id+9999})
-        response = self.client.get(url, follow=True)
-        response_url = reverse('user_list')
-        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
-        self.assertTemplateUsed(response, 'user_list.html')
