c82c1bc214dae715a4bec4fd970631e8c0732b25
Zakariya Ahmed Mohamed <zakariya.ahmed_mohamed@kcl.ac.uk>
2022-03-22 19:17:33 +0000

    Reinstate deleted password_reset tests

--- bookclub/tests/views/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def test_password_reset_confirm_changes_password(self):
-        token = response.context['token']
-        uid = response.context['uid']
-        password_response = self.client.post(reverse('activate', kwargs={'token':token,'uidb64':uid}))
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].to, [self.user.email])
