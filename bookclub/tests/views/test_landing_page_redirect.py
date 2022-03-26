b04480e82679393bb6bd603912289c3ffd7a2776
Zishan Rahman <zishanfrahman@outlook.com>
2022-03-26 09:48:17 +0000

    Expand a landing page test to check if user is authenticated or not (they shouldn't be)

--- bookclub/tests/views/test_landing_page_redirect.py
+++ bookclub/tests/views/test_landing_page_redirect.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from bookclub.models import User
+from django.contrib import auth
 
 class LandingPageRedirectTestCase(TestCase):
     """Test(s) to see if the landing page redirects to the login page when logged in"""
@@ -18,11 +19,15 @@ class LandingPageRedirectTestCase(TestCase):
         response = self.client.get(reverse('landing_page'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('landing_page.html')
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated)
     
     def test_landing_page_redirects_to_home_when_user_logged_in(self):
         redirect_url = reverse('home')
         self.client.login(email=self.user.email, password=self.user.password)
         response = self.client.get(reverse('landing_page'), follow=False)
+        # user = auth.get_user(self.client)
+        # self.assertTrue(user.is_authenticated) #still returns False when it shouldn't
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed('home.html')
     
