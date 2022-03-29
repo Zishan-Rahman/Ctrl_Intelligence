a4daebfd58e576243c54a4ce48c9a1b140d4d29a
Zishan Rahman <zishanfrahman@outlook.com>
2022-03-29 20:18:14 +0100

    Remove some dead (unused) code

--- bookclub/views/application_views.py
+++ bookclub/views/application_views.py
@@ -2,7 +2,6 @@
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render, redirect
-# from bookclub.forms import ApplicantForm, ApplicationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
