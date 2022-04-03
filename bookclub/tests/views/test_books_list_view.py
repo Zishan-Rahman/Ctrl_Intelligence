6771b501c9abf2b458b4424281f277b54b520409
19025208 <91200797+19025208@users.noreply.github.com>
2022-04-03 13:23:23 +0100

    Update test_books_list_view.py

--- bookclub/tests/views/test_books_list_view.py
+++ bookclub/tests/views/test_books_list_view.py
@@ -74,7 +74,7 @@ class BooksListViewTestCase(TestCase, LogInTester):
         self.assertFalse(page_obj.has_next())
 
     def test_book_list_view_has_remove_from_reading_list_button_when_book_is_in_reading_list(self):
-"       ""Testing for remove from reading list button."""
+       """"Testing the remove from reading list button."""
         self.client.login(email=self.user.email, password='Password123')
         self.user.currently_reading_books.add(self.book)
         response = self.client.get(self.url)
@@ -83,7 +83,7 @@ class BooksListViewTestCase(TestCase, LogInTester):
                       f'class="bi bi-bookmarks-fill"></i></button></td>', html)
 
     def test_book_list_view_has_add_to_reading_list_button_when_book_is_not_in_reading_list(self):
-"       ""Testing for add to reading list button."""
+"       ""Testing the add to reading list button."""
         self.client.login(email=self.user.email, password='Password123')
         response = self.client.get(self.url)
         html = response.content.decode('utf8')
