a95bdff80ecd4473b2638a05f42304cd54b89b4e
Suhayb <91222617+SuhaybYY@users.noreply.github.com>
2022-04-04 07:03:56 +0400

    Update test_books_list_view.py

--- bookclub/tests/views/test_books_list_view.py
+++ bookclub/tests/views/test_books_list_view.py
@@ -132,6 +132,19 @@ class BooksListViewTestCase(TestCase, LogInTester):
         after_reading_list_count = self.user.favourite_books.count()
         self.assertNotEqual(before_reading_list_count, after_reading_list_count)
 
+    def test_favourite_button_in_book_list_works(self):
+        """Testing if book list favourite button works."""
+        self.client.login(email=self.user.email, password='Password123')
+        before_reading_list_count = self.user.favourite_books.count()
+        response = self.client.get('/book_list/1/favourite', follow=True)
+        redirect_url = '/books/'
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        messages_list = list(response.context['messages'])
+        self.assertEqual(len(messages_list), 1)
+        self.assertEqual(messages_list[0].level, messages.SUCCESS)
+        after_reading_list_count = self.user.favourite_books.count()
+        self.assertNotEqual(before_reading_list_count, after_reading_list_count)
+
     def _is_logged_in(self):
         """Testing if logged in."""
         return '_auth_user_id' in self.client.session.keys()
