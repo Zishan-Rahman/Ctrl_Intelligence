e307d49dc87ef8025292e5597648a3942609224b
Zishan Rahman <zishanfrahman@outlook.com>
2022-04-04 18:14:23 +0100

    Remove POST method from ReadingListView

--- bookclub/views/book_views.py
+++ bookclub/views/book_views.py
@@ -72,15 +72,6 @@ class ReadingListView(LoginRequiredMixin, ListView):
         context['books'] = books
         return context
 
-    def post(self, request, *args, **kwargs):
-        user_id = self.kwargs['user_id']
-        user = User.objects.get(id=user_id)
-        books = user.currently_reading_books.all()
-        paginator = Paginator(books, settings.BOOKS_PER_PAGE)
-        page_number = self.request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        return render(request, 'reading_list.html', {"books": books, "user": user, "page_obj": page_obj})
-
     def get(self, request, *args, **kwargs):
         user_id = self.kwargs['user_id']
         user = User.objects.get(id=user_id)
