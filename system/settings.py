cbf8d936b21e5eabbc7ca467c938e28f955319ab
Zakariya Ahmed Mohamed <zak2mahmed@gmail.com>
2023-11-12 17:23:22 +0000

    Update the App Password

--- system/settings.py
+++ system/settings.py
@@ -163,7 +163,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'bookwise0000@gmail.com'
-EMAIL_HOST_PASSWORD = 'xadxrneeznkqtewt'
+EMAIL_HOST_PASSWORD = 'pvkyuelsasxebwt'
 DEFAULT_FROM_EMAIL = 'bookwise0000@gmail.com'
 SERVER_EMAIL = 'bookwise0000@gmail.com'
 
